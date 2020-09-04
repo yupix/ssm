2fcc3a22a8f2efe550a2b5a3098e1a2b85a83bd9
yupix <yupi0982@outlook.jp>
2020-09-04 15:54:14 +0900

    feat: add default system

---
+++ cogs/testcog.py
@@ -0,0 +1,40 @@
+import discord
+from discord.ext import commands
+
+
+class TestCog(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def ping(self, ctx):
+        await ctx.send('pong!')
+
+    @commands.command()
+    async def what(self, ctx, what):
+        await ctx.send(f'{what}とはなんですか?')
+
+    @commands.group()
+    async def role(self, ctx):
+        if ctx.invoked_subcommand is None:
+            await ctx.send('this command is sub option required')
+
+    @role.command()
+    async def add(self, ctx, member: discord.Member, role: discord.Role):
+        await member.add_roles(role)
+
+    @role.command()
+    async def remove(self, ctx, member: discord.Member, role: discord.Role):
+        await member.remove_roles(role)
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.author.bot:
+            return
+        if message.content == 'こんにちは':
+            await message.channel.send(f'{message.author.name}さんこんにちは！')
+
+
+def setup(bot):
+    bot.add_cog(TestCog(bot))
