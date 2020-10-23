cdd47c5e117e6c1cc0fcc8da85c71b0d55d782f5
yupix <yupi0982@outlook.jp>
2020-10-23 22:58:26 +0900

    feat: Succeeded in separating blog and user data

--- cogs/blogcog.py
+++ cogs/blogcog.py
@@ -12,6 +12,85 @@ from main import mycursor, mydb, embed_send, db_search, db_delete, db_reformat, 
     db_insert
 
 
+async def blog_level_system(ctx, execution_user_id, type):
+    if type == 0:
+        info_table_name = 'blog_sub_info'
+        xp_info_table_name = 'blog_channel_xp'
+        channnel_insert_id_column = 'id'
+        default_levelup_xp = 7  # 基本レベルを設定
+    elif type == 1:
+        info_table_name = 'private_blog_user'
+        xp_info_table_name = 'private_blog_user_xp'
+        channnel_insert_id_column = 'channel_insert_id'
+        default_levelup_xp = 5  # 基本レベルを設定
+
+    ##
+    # 紐づけされているIDを各種取得
+    ##
+    search_main_info_insert_id = await db_reformat(await db_search('id', 'blog_main_info', f'category_id = {ctx.channel.category.id}'), 1)
+
+    if search_main_info_insert_id:
+
+        ##
+        # 紐づけされているIDを各種取得
+        ##
+        search_main_info_insert_id = await db_reformat(
+            await db_search('id', 'blog_main_info', f'category_id = {ctx.channel.category.id}'), 1)
+        search_sub_info_insert_id = await db_reformat(
+            await db_search('id', 'blog_sub_info', f'main_info_insert_id = {search_main_info_insert_id}'), 1)
+        search_private_blog_user_insert_id = await db_reformat(
+            await db_search('id', 'private_blog_user', f'channel_insert_id = {search_sub_info_insert_id}'), 1)
+
+        # 投稿数をデータベースから取得
+        search_info_table_number_of_posts = await db_reformat(await db_search('number_of_posts', f'{info_table_name}', f'{channnel_insert_id_column} = {ctx.channel.id} AND number_of_posts >= 0'), 1)
+
+        # 経験値をデータベースから取得
+        db_get_exp = await db_search('xp', f'{xp_info_table_name}', f'channel_insert_id = {search_sub_info_insert_id} AND xp >= 0')
+        reformat_xp = await db_reformat(db_get_exp, 1)
+        # レベルをデータベースから取得
+        db_get_level = await db_search('level', f'{xp_info_table_name}',
+                                       f'channel_insert_id = {search_sub_info_insert_id} AND level >= 0')
+        reformat_level = await db_reformat(db_get_level, 2)
+
+        if reformat_level == 1:
+            next_levelup_xp = float(default_levelup_xp * 1.1) + int(reformat_level * 2) / 2
+        else:
+            db_get_saved_levelup_xp = await db_search('saved_levelup_xp', f'{xp_info_table_name}',
+                                                      f'channel_id = {ctx.channel.id} AND saved_levelup_xp IS NOT NULL')
+            reformat_saved_levelup_xp = await db_reformat(db_get_saved_levelup_xp, 3)
+            print(reformat_saved_levelup_xp)
+            reformat_saved_levelup_xp = float(reformat_saved_levelup_xp)
+            next_levelup_xp = float(reformat_saved_levelup_xp * 1.1) + int(reformat_level * 2) / 2
+        rereformat_xp = int(reformat_xp) / 100
+        rereformat_xp = str(rereformat_xp)
+        rereformat_xp = rereformat_xp[:rereformat_xp.find('.')]
+
+        next_levelup_xp = str(next_levelup_xp)
+        next_levelup_xp = next_levelup_xp[:next_levelup_xp.find('.')]
+        print(f'レベルアップに必要な経験値: {next_levelup_xp}\n現在の経験値{rereformat_xp}')
+        if int(rereformat_xp) >= int(next_levelup_xp):
+            val = (f"{next_levelup_xp}", f"{search_sub_info_insert_id}")
+            await db_update(f'{xp_info_table_name}', 'saved_levelup_xp = %s WHERE channel_insert_id = %s', val)
+
+            reformat_level = int(reformat_level) + 1
+            embed = discord.Embed(
+                title=f"レベルアップ!", color=0x8bc34a)
+            embed.add_field(name=f"blogのレベルが{reformat_level}に上がりました!", value=f"今後もblogでのご活躍をご期待しています!",
+                            inline=True)
+            await ctx.channel.send(embed=embed)
+            next_xp = '0'
+        else:
+            next_xp = int(reformat_xp) + + int(2) * 10
+            print(f'今回の発言後のxp(小数) {next_xp / 100}\n今回の発言後のxp(整数) {next_xp}')
+        next_number_of_posts = int(search_info_table_number_of_posts) + 1
+
+        val = (f"{int(next_xp)}", f"{reformat_level}", f"{search_sub_info_insert_id}")
+        await db_update(f'{xp_info_table_name}', 'xp = %s, level = %s WHERE channel_insert_id = %s', val)
+
+        val = (f"{next_number_of_posts}", f"{ctx.channel.id}")
+        await db_update(f'{info_table_name}', 'number_of_posts = %s WHERE channel_id = %s', val)
+
+
 class BlogCog(commands.Cog):
 
     def __init__(self, bot):
@@ -79,20 +158,25 @@ class BlogCog(commands.Cog):
         if len(db_search_category_id) == 1:
             db_search_channel_id = await db_search('channel_id', 'blog_sub_info',
                                                    f'channel_id = {ctx.channel.id}')
+
+            search_main_info_insert_id = await db_reformat(
+                await db_search('id', 'blog_main_info', f'category_id = {ctx.channel.category.id}'), 1) #
+            # データベースから挿入されたidを取得し、文字型を変更
+
             if len(db_search_channel_id) == 0:
-                sql = "INSERT INTO blog_sub_info (channel_id, embed_color, number_of_posts) VALUES (%s, %s, %s)"
+                sql = "INSERT INTO blog_sub_info (main_info_insert_id, channel_id, embed_color, number_of_posts) VALUES (%s, %s, %s, %s)"
                 embed_color_list = [5620992, 16088855, 16056193, 9795021]
                 print(random.choice(embed_color_list))
-                val = (f'{ctx.channel.id}', random.choice(embed_color_list), 0)
+                val = (f'{search_main_info_insert_id}', f'{ctx.channel.id}', random.choice(embed_color_list), 0)
                 await db_insert(sql, val)
 
                 db_search_insert_id = await db_search('id', 'blog_sub_info',
-                                                       f'channel_id = {ctx.channel.id}')
+                                                      f'channel_id = {ctx.channel.id}')
                 reformat_db_search_insert_id = await db_reformat(db_search_insert_id, 1)
 
                 # 実行したユーザーの基本情報を設定
-                sql = "INSERT INTO private_blog_user (channel_insert_id, user_id, number_of_posts) VALUES (%s, %s, %s)"
-                val = (f'{reformat_db_search_insert_id}', f'{ctx.author.id}', 0)
+                sql = "INSERT INTO private_blog_user (channel_insert_id, user_id, number_of_posts, role) VALUES (%s, %s, %s, %s)"
+                val = (f'{reformat_db_search_insert_id}', f'{ctx.author.id}', 0, 'owner')
                 await db_insert(sql, val)
 
                 # チャンネルのレベルに関する初期情報を設定
@@ -101,7 +185,7 @@ class BlogCog(commands.Cog):
                 await db_insert(sql, val)
 
                 db_search_user_insert_id = await db_search('id', 'private_blog_user',
-                                                       f'user_id = {ctx.author.id}')
+                                                           f'user_id = {ctx.author.id}')
                 reformat_db_search_user_insert_id = await db_reformat(db_search_user_insert_id, 1)
 
                 # 登録を実行したユーザーのレベルに関する初期情報を設定
@@ -256,62 +340,14 @@ class BlogCog(commands.Cog):
     async def on_message(self, ctx):
         if ctx.author.bot:
             return
-        if ctx.content != f'{bot_prefix}blog status':
+        if ctx.content != f'{bot_prefix}blog status': # blog statusコマンドが投稿にカウントされないように
             if len(ctx.content) >= 3:
                 myresult = await db_search('channel_id', 'blog_sub_info', f'channel_id = {ctx.channel.id}')
+                execution_user_id = f'{ctx.author.id}'
                 if len(myresult) >= 1:
-                    # 投稿数をデータベースから取得
-                    db_get_number_of_posts = await db_search('number_of_posts', 'blog_sub_info',
-                                                             f'channel_id = {ctx.channel.id} AND number_of_posts >= 0')
-                    reformat_number_of_posts = await db_reformat(db_get_number_of_posts, 1)
-
-                    # 経験値をデータベースから取得
-                    db_get_exp = await db_search('xp', 'discord_blog_xp', f'channel_id = {ctx.channel.id} AND xp >= 0')
-                    reformat_xp = await db_reformat(db_get_exp, 1)
-                    # レベルをデータベースから取得
-                    db_get_level = await db_search('level', 'discord_blog_xp',
-                                                   f'channel_id = {ctx.channel.id} AND level >= 0')
-                    reformat_level = await db_reformat(db_get_level, 2)
-
-                    default_levelup_xp = 5  # 基本レベルを設定
-
-                    if reformat_level == 1:
-                        next_levelup_xp = float(default_levelup_xp * 1.1) + int(reformat_level * 2) / 2
-                    else:
-                        db_get_saved_levelup_xp = await db_search('saved_levelup_xp', 'discord_blog_xp',
-                                                                  f'channel_id = {ctx.channel.id} AND saved_levelup_xp IS NOT NULL')
-                        reformat_saved_levelup_xp = await db_reformat(db_get_saved_levelup_xp, 3)
-                        print(reformat_saved_levelup_xp)
-                        reformat_saved_levelup_xp = float(reformat_saved_levelup_xp)
-                        next_levelup_xp = float(reformat_saved_levelup_xp * 1.1) + int(reformat_level * 2) / 2
-                    rereformat_xp = int(reformat_xp) / 100
-                    rereformat_xp = str(rereformat_xp)
-                    rereformat_xp = rereformat_xp[:rereformat_xp.find('.')]
-
-                    next_levelup_xp = str(next_levelup_xp)
-                    next_levelup_xp = next_levelup_xp[:next_levelup_xp.find('.')]
-                    print(f'レベルアップに必要な経験値: {next_levelup_xp}\n現在の経験値{rereformat_xp}')
-                    if int(rereformat_xp) >= int(next_levelup_xp):
-                        val = (f"{next_levelup_xp}", f"{ctx.channel.id}")
-                        await db_update('discord_blog_xp', 'saved_levelup_xp = %s WHERE channel_id = %s', val)
-
-                        reformat_level = int(reformat_level) + 1
-                        embed = discord.Embed(
-                            title=f"レベルアップ!", color=0x8bc34a)
-                        embed.add_field(name=f"blogのレベルが{reformat_level}に上がりました!", value=f"今後もblogでのご活躍をご期待しています!",
-                                        inline=True)
-                        await ctx.channel.send(embed=embed)
-                        next_xp = '0'
-                    else:
-                        next_xp = int(reformat_xp) + + int(2) * 10
-                        print(f'今回の発言後のxp(小数) {next_xp / 100}\n今回の発言後のxp(整数) {next_xp}')
-                    next_number_of_posts = int(reformat_number_of_posts) + 1
-
-                    val = (f"{int(next_xp)}", f"{reformat_level}", f"{ctx.channel.id}")
-                    await db_update('discord_blog_xp', 'xp = %s, level = %s WHERE channel_id = %s', val)
-
-                    val = (f"{next_number_of_posts}", f"{ctx.channel.id}")
-                    await db_update('blog_sub_info', 'number_of_posts = %s WHERE channel_id = %s', val)
+                    print(f'{myresult}')
+                    await blog_level_system(ctx, execution_user_id, 0)
+                    await blog_level_system(ctx, execution_user_id, 1)
 
 
 def setup(bot):
