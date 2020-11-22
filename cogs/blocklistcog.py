51ab619ec78b876c44d437e4e96023a6f19d3878
yupix <yupi0982@outlook.jp>
2020-11-22 22:42:45 +0900

    feat: check if block list mode exists

--- cogs/blocklistcog.py
+++ cogs/blocklistcog.py
@@ -4,9 +4,8 @@ import discord
 import typing
 from discord import NotFound, HTTPException
 from discord.ext import commands
-
 from main import db_search, mycursor, mydb, db_reformat, db_delete, embed_send, db_insert, db_get_auto_increment, \
-    bot_prefix
+    bot_prefix, logger
 
 
 async def blocklist_informations(ctx, user):
@@ -15,6 +14,7 @@ async def blocklist_informations(ctx, user):
     search_blocklist_user_id = await db_reformat(await db_search('user_id', 'blocklist_user', f'user_id = {user.id}'), 2)  # user_idを取得
 
     return search_blocklist_id, search_blocklist_server_id, search_blocklist_user_id
+
 
 async def blog_reaction(reaction, reformat_mode, user, msg, reformat_check_reaction):
     if reformat_mode == '0':
@@ -88,59 +88,70 @@ class BlocklistCog(commands.Cog):
                 await db_insert(sql, val)
             search_settings_server_id = await db_reformat(
                 await db_search('server_id', 'blocklist_settings', f'server_id = {ctx.guild.id}'), 2)
-            if not search_settings_server_id: # 既に設定が存在するか確認
-                if f'{mode}' == 'autokick' or f'{mode}' == 'autoban' or f'{mode}' == 'nonerole' or f'{mode}' == 'addrole': # 存在するmodeか確認
-                    sql = "INSERT INTO blocklist_settings (server_id, mode) VALUES (%s, %s)"
-                    val = (ctx.guild.id, f'{mode}')
-                    if f'{mode}' == 'nonerole' or f'{mode}' == 'addrole':
-                        role = self.bot.get_role(args[0])
-                        print(role.id)
-                        sql = "INSERT INTO blocklist_role (server_id, role_id) VALUES (%s, %s)"
-                        val = (ctx.guild.id, role.id)
+
+            if not search_settings_server_id:  # 既に設定が存在するか確認
+                processing_mode_list = ['autokick', 'autoban', 'addrole', 'removerole']
+                for check_mode in processing_mode_list:
+                    if f'{mode}' == f'{check_mode}':  # 存在するmodeか確認
+                        logger.debug('モードの確認に成功しました')
+                        sql = "INSERT INTO blocklist_settings (server_id, mode) VALUES (%s, %s)"
+                        val = (ctx.guild.id, f'{mode}')
+                        if f'{mode}' == 'nonerole' or f'{mode}' == 'addrole':
+                            role = self.bot.get_role(args[0])
+                            print(role.id)
+                            sql = "INSERT INTO blocklist_role (server_id, role_id) VALUES (%s, %s)"
+                            val = (ctx.guild.id, role.id)
+                            await db_insert(sql, val)
                         await db_insert(sql, val)
-                    await db_insert(sql, val)
-                    await embed_send(ctx, self.bot, 0, '成功', '設定を保存しました!')
+                        await embed_send(ctx, self.bot, 0, '成功', '設定を保存しました!')
+                        break
                 else:
                     await embed_send(ctx, self.bot, 1, 'エラー', '存在しないモードです')
             else:
                 await embed_send(ctx, self.bot, 1, 'エラー', '既に設定が存在します')
+
+
     @blocklist.command()
     async def add(self, ctx, user_id, mode: typing.Optional[str] = None):
         if ctx.author.guild_permissions.administrator:
             db_search_server_id = await db_reformat(await db_search('server_id', 'blocklist_server', f'server_id = {ctx.guild.id}'), 2)
-            if db_search_server_id: # サーバーIDが既に登録されてるか確認
-                try:
-                    user = await self.bot.fetch_user(user_id)
-                    search_blocklist_id, search_blocklist_server_id, search_blocklist_user_id = await blocklist_informations(ctx, user)
-                    print(search_blocklist_id, search_blocklist_server_id, search_blocklist_user_id)
-                    if not search_blocklist_user_id:
-                        embed = discord.Embed(title=f"{user.name} をブロックリストに登録しますか？",
-                                              description="怒りに我を忘れていないかもう一度冷静になって確認してみましょう\n問題が無いようなら✅を押してください。",
-                                              color=0xffc629)
-                        msg = await ctx.send(embed=embed)
-                        await msg.add_reaction('✅')
-                        await msg.add_reaction('✖')
+            if mode is not None:
+                processing_mode_list = ['autokick', 'autoban', 'addrole', 'removerole']
+                for check_mode in processing_mode_list:
+                    if f'{mode}' == f'{check_mode}':  # 存在するmodeか確認
+                        if db_search_server_id:  # サーバーIDが既に登録されてるか確認
+                            try:
+                                user = await self.bot.fetch_user(user_id)
+                                search_blocklist_id, search_blocklist_server_id, search_blocklist_user_id = await blocklist_informations(ctx, user)
+                                print(search_blocklist_id, search_blocklist_server_id, search_blocklist_user_id)
+                                if not search_blocklist_user_id:
+                                    embed = discord.Embed(title=f"{user.name} をブロックリストに登録しますか？",
+                                                          description="怒りに我を忘れていないかもう一度冷静になって確認してみましょう\n問題が無いようなら✅を押してください。",
+                                                          color=0xffc629)
+                                    msg = await ctx.send(embed=embed)
+                                    await msg.add_reaction('✅')
+                                    await msg.add_reaction('✖')
 
-                        print(msg.id)
-                        sql = "INSERT INTO reactions (message_id) VALUES (%s)"
-                        val = (msg.id,)
-                        await db_insert(sql, val)
+                                    sql = "INSERT INTO reactions (message_id) VALUES (%s)"
+                                    val = (msg.id,)
+                                    await db_insert(sql, val)
 
-                        search_reaction_id = await db_reformat(await db_search('id', 'reactions', f'message_id = {msg.id}'), 2)
+                                    search_reaction_id = await db_reformat(await db_search('id', 'reactions', f'message_id = {msg.id}'), 2)
+                                    sql = "INSERT INTO blocklist_reaction (reaction_id, server_id, channel_id, user_id, command, mode, block_mode) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+                                    val = (search_reaction_id, msg.guild.id, msg.channel.id, user.id, 'blocklist', 0, f'{mode}')
+                                    await db_insert(sql, val)
 
-                        sql = "INSERT INTO blocklist_reaction (reaction_id, server_id, channel_id, user_id, command, mode, block_mode) VALUES (%s, %s, %s, %s, %s, %s, %s)"
-                        val = (search_reaction_id, msg.guild.id, msg.channel.id, user.id, 'blocklist', 0, f'{mode}')
-                        await db_insert(sql, val)
-
-                    else:
-                        await embed_send(ctx, self.bot, 1, 'エラー', '既に登録されているユーザーです')
-                except NotFound:
-                    await embed_send(ctx, self.bot, 1, 'エラー', '存在しないユーザーです')
-                except HTTPException:
-                    await embed_send(ctx, self.bot, 1, 'エラー', '取得時にエラーが発生しました')
-            else:
-                await embed_send(ctx, self.bot, 1, 'エラー', f'サーバーが登録されていません。\n`{bot_prefix}blocklist register mode role`\nを実行した後に再度実行してください。')
-
+                                else:
+                                    await embed_send(ctx, self.bot, 1, 'エラー', '既に登録されているユーザーです')
+                            except NotFound:
+                                await embed_send(ctx, self.bot, 1, 'エラー', '存在しないユーザーです')
+                            except HTTPException:
+                                await embed_send(ctx, self.bot, 1, 'エラー', '取得時にエラーが発生しました')
+                        else:
+                            await embed_send(ctx, self.bot, 1, 'エラー', f'サーバーが登録されていません。\n`{bot_prefix}blocklist register mode role`\nを実行した後に再度実行してください。')
+                        break
+                else:
+                    await embed_send(ctx, self.bot, 1, 'エラー', '存在しないモードです')
     @blocklist.command()
     async def remove(self, ctx, user_id):
         if ctx.author.guild_permissions.administrator:
@@ -177,13 +188,15 @@ class BlocklistCog(commands.Cog):
             search_block_id = await db_search('block_id', 'blocklist_user', f'server_id = {ctx.guild.id}')
             search_user_id = await db_search('user_id', 'blocklist_user', f'server_id = {ctx.guild.id}')
             #search_datatime = await db_reformat(await db_search('user_id', 'blocklists', f'id = {ctx.guild.id}'), 2)
-            count = 0
-            for block_id in search_block_id:
-                count += 1
-                print(count)
-                print(block_id)
 
-            print(f'{search_block_id}', f'{search_user_id}')
+            for i in range(len(search_block_id)):
+
+                print(search_block_id)
+                block_id = await db_reformat(f'{search_block_id[-i]}', 1)
+                user_id = await db_reformat(f'{search_user_id[-i]}', 1)
+                print(f'{block_id}', f'{user_id}')
+                print(''.join((2), (1)))
+
         else:
             await ctx.send('このコマンドには管理者必須やで')
 
