e591223aabc85d17aa0d568b7155b9dbcd93e47f
yupix <yupi0982@outlook.jp>
2020-12-06 22:04:50 +0900

    ✨feature: add grobal command #15

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import os
 import time
 import urllib
 
-import coloredlogs
 import discord
 import mysql.connector
 import traceback
@@ -14,7 +13,7 @@ from discord.ext import commands
 from discord.ext.commands import CommandNotFound
 from halo import Halo
 from logging import getLogger, StreamHandler, DEBUG, Formatter, addLevelName
-
+from modules.create_logger import easy_logger
 
 config_ini = configparser.ConfigParser(os.environ)
 config_ini.read('./config.ini', encoding='utf-8')
@@ -48,48 +47,7 @@ Spped = config_ini['JTALK']['Spped']
 # loggerオブジェクトの宣言
 logger = getLogger(__name__)
 
-# loggerのログレベル設定(ハンドラに渡すエラーメッセージのレベル)
-logger.setLevel(DEBUG)
-
-# --------------------------------
-# 2.handlerの設定
-# --------------------------------
-# handlerの生成
-stream_handler = StreamHandler()
-
-# handlerのログレベル設定(ハンドラが出力するエラーメッセージのレベル)
-stream_handler.setLevel(DEBUG)
-
-# --------------------------------
-# 3.loggerにhandlerをセット
-# --------------------------------
-logger.addHandler(stream_handler)
-logger.propagate = False
-
-coloredlogs.CAN_USE_BOLD_FONT = True
-coloredlogs.DEFAULT_FIELD_STYLES = {'asctime': {'color': 'green'},
-									'hostname': {'color': 'magenta'},
-									'levelname': {'color': 'black', 'bold': True},
-									'name': {'color': 'blue'},
-									'programname': {'color': 'cyan'}
-									}
-coloredlogs.DEFAULT_LEVEL_STYLES = {'critical': {'color': 'red', 'bold': True},
-									'error': {'color': 'red'},
-									'warning': {'color': 'yellow'},
-									'notice': {'color': 'magenta'},
-									'info': {'color': 'green'},
-									'debug': {},
-									'spam': {'color': 'green', 'faint': True},
-									'success': {'color': 'green', 'bold': True},
-									'verbose': {'color': 'blue'}
-									}
-
-coloredlogs.install(level='DEBUG', logger=logger, fmt='%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%Y/%m/%d %H:%M:%S')
-
-# SUCCESSを追加
-logger.SUCCESS = 25 # WARNINGとINFOの間
-addLevelName(logger.SUCCESS, 'SUCCESS')
-setattr(logger, 'success', lambda message, *args: logger._log(logger.SUCCESS, message, args))
+logger = easy_logger.create(logger)
 
 while True:
 	try:
@@ -122,8 +80,8 @@ INITIAL_EXTENSIONS = [
 	'cogs.note',
 	'cogs.emoji',
 	'cogs.pso2',
-	'cogs.rss'
-
+	'cogs.rss',
+	'cogs.level'
 ]
 
 def add_list(hit, key, args_list):
