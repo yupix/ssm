05e7c04c40461362b4645c67a2034ec0fed44a94
yupix <yupi0982@outlook.jp>
2021-03-13 13:32:46 +0900

    feat: リアクションに関する下準備 #41

--- sql/models/basic.py
+++ sql/models/basic.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, ForeignKey, Integer, String, INT, VARCHAR, BIGINT, Table
+from sqlalchemy import Column, ForeignKey, Integer, String, INT, VARCHAR, BIGINT, Table, UniqueConstraint, JSON
 from sqlalchemy.orm import relationship
 
 from settings import Base, engine
@@ -6,12 +6,15 @@ from settings import Base, engine
 
 class Reactions(Base):
     __tablename__ = 'reactions'
-    id = Column(Integer, autoincrement=True, primary_key=True)
-    message_id = Column(BIGINT, primary_key=True)
+    __table_args__ = (UniqueConstraint('guild_id','channel_id', 'message_id'),)
 
-    @property
-    def serialize(self):
-        """Return object data in easily serializeable format"""
-        return {
-            'message_id': self.message_id,
-        }
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    guild_id = Column(BIGINT)
+    channel_id = Column(BIGINT)
+    message_id = Column(BIGINT)
+    type = Column(VARCHAR(255))
+    action = Column(VARCHAR(255))
+    content = Column(JSON)
+    module_path = Column(VARCHAR(255))
+    class_name = Column(VARCHAR(255))
+
