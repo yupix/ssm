d36af351e08933aca770fdba723ed685a99e430f
yupix <yupi0982@outlook.jp>
2021-03-14 21:06:48 +0900

    chore: noteモジュールの更新

--- sql/models/note.py
+++ sql/models/note.py
@@ -5,41 +5,33 @@ from settings import Base, engine
 
 
 class NotesUser(Base):
-    __tablename__ = 'notes_user'
+	__tablename__ = 'notes_user'
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    user_id = Column(BIGINT, unique=True)
+	id = Column(Integer, primary_key=True, autoincrement=True)
+	user_id = Column(BIGINT, unique=True)
 
-    @property
-    def serialize(self):
-        """Return object data in easily serializeable format"""
-        return {
-            'id': self.id,
-            'user_id': self.user_id,
-        }
+	def __repl__(self):
+		return f'NotesUser(id={self.id}, user_id={self.user_id})'
 
 
 class NotesCategory(Base):
-    __tablename__ = 'notes_category'
-    id = Column(BIGINT, primary_key=True, autoincrement=True)
-    category_name = Column(VARCHAR(255), unique=True)
+	__tablename__ = 'notes_category'
+	id = Column(BIGINT, primary_key=True, autoincrement=True)
+	category_name = Column(VARCHAR(255), unique=True)
+
+	def __repl__(self):
+		return f'NotesCategory(id={self.id}, category_name={self.category_name})'
 
 
 class NotesDetail(Base):
-    __tablename__ = 'notes_detail'
-    __table_args__ = (UniqueConstraint('user_id', 'content', 'category_name'),)
+	__tablename__ = 'notes_detail'
+	__table_args__ = (UniqueConstraint('user_id', 'content', 'category_name'),)
 
-    id = Column(BIGINT, primary_key=True, autoincrement=True)
-    user_id = Column(BIGINT, ForeignKey('notes_user.user_id', onupdate='CASCADE', ondelete='CASCADE'))
-    content = Column(VARCHAR(255))
-    category_name = Column(VARCHAR(255))
+	id = Column(BIGINT, primary_key=True, autoincrement=True)
+	user_id = Column(BIGINT, ForeignKey('notes_user.user_id', onupdate='CASCADE', ondelete='CASCADE'))
+	content = Column(VARCHAR(255))
+	category_name = Column(VARCHAR(255))
 
-    @property
-    def serialize(self):
-        """Return object data in easily serializeable format"""
-        return {
-            'id': self.id,
-            'user_id': self.user_id,
-            'content': self.content,
-            'category_name': self.category_name,
-        }
+	def __repl__(self):
+		return f'NotesDetail(id={self.id}, user_id={self.user_id}, content={self.content}, category_name={self.category_name})'
+
