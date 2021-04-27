9d39d3b02f84405a2fdb629d3d9bcaa4502c9e04
yupix <yupi0982@outlook.jp>
2021-04-27 22:37:10 +0900

    feat: ブログを修正するにあたってモデルを変更

--- ssm/sql/models/blog.py
+++ ssm/sql/models/blog.py
@@ -1,20 +1,31 @@
-from sqlalchemy import Column, ForeignKey, Integer, String, INT, VARCHAR, BIGINT, Table, UniqueConstraint, ForeignKeyConstraint
+from sqlalchemy import Column, ForeignKey, Integer, BIGINT, UniqueConstraint, JSON
 from sqlalchemy.orm import relationship
 
 from ssm import Base
 
 
 class BlogsServer(Base):
-    __tablename__ = 'blogs_server'
+	__tablename__ = 'blogs_server'
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    server_id = Column(BIGINT, unique=True)
-    blogs_category = relationship("BlogsCategory", backref='blogs_server', lazy='dynamic')
+	id = Column(Integer, primary_key=True, autoincrement=True)
+	server_id = Column(BIGINT, unique=True)
+	blogs_category = relationship("BlogsCategory", backref='blogs_server', lazy='dynamic')
+
 
 class BlogsCategory(Base):
-    __tablename__ = 'blogs_category'
-    __table_args__ = (UniqueConstraint('server_id', 'category_id'),)
+	__tablename__ = 'blogs_category'
+	__table_args__ = (UniqueConstraint('category_id'),)
 
-    id = Column(BIGINT, primary_key=True, autoincrement=True)
-    server_id = Column(BIGINT, ForeignKey('blogs_server.server_id', onupdate='CASCADE', ondelete='CASCADE'))
-    category_id = Column(BIGINT)
+	id = Column(BIGINT, primary_key=True, autoincrement=True)
+	server_id = Column(BIGINT, ForeignKey('blogs_server.server_id', onupdate='CASCADE', ondelete='CASCADE'))
+	category_id = Column(BIGINT)
+	blogs_channel = relationship("BlogsChannel", backref='blogs_category', lazy='dynamic')
+
+
+class BlogsChannel(Base):
+	__tablename__ = 'blogs_channel'
+	id = Column(Integer, primary_key=True, autoincrement=True)
+	category_id = Column(BIGINT, ForeignKey('blogs_category.category_id', onupdate='CASCADE', ondelete='CASCADE'))
+	channel_id = Column(BIGINT, unique=True)
+	owner_id = Column(BIGINT)
+	sub_user_list = Column(JSON)
