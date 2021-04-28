e2d92058653b2a6b5e6d835adec4c0d435c9c641
yupix <yupi0982@outlook.jp>
2021-04-28 18:31:06 +0900

    feat: embedでoptionは非必須に、画像等を追加できるように

--- ssm/modules/embed_manager.py
+++ ssm/modules/embed_manager.py
@@ -8,7 +8,8 @@ class EmbedManager:
 	def __init__(self):
 		pass
 
-	async def generate(self, embed_title: str = None, embed_description: str = None, mode: str = None, embed_color: int = 0x8bc34a, embed_content: list = None):
+	async def generate(self, embed_title: str = None, embed_description: str = None, mode: str = None, embed_color: int = 0x8bc34a, embed_content: list = None, embed_image: str = None,
+	                   embed_thumbnail: str = None):
 		"""{'title': 'content', 'value': 'content', 'option': {'inline': 'False'}}"""
 		if mode is not None:
 			if mode == 'succeed':
@@ -17,10 +18,15 @@ class EmbedManager:
 				embed_color = 0xd32f2f
 
 		embed = discord.Embed(title=embed_title, description=embed_description, color=embed_color)
+		if embed_image is not None:
+			embed.set_image(url=f'{embed_image}')
+		if embed_thumbnail is not None:
+			embed.set_thumbnail(url=embed_thumbnail)
+
 		for content in embed_content:
 			title = content.get('title', 'タイトルが指定されていません')
 			value = content.get('value', None)
-			inline = content.get('option').get('inline')
+			inline = content.get('option', {'inline': 'True'}).get('inline')
 			print(f"{content['title']}, {content['value']}")
 			embed.add_field(name=title, value=value, inline=bool(strtobool(inline)))
 		return embed
