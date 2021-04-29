927c6f3ed577c0ca975a96386668d29881c9b05a
yupix <yupi0982@outlook.jp>
2021-04-29 20:47:13 +0900

    feat: 実績を達成したかの処理だけ追加 #63

--- ssm/cogs/blog.py
+++ ssm/cogs/blog.py
@@ -1,9 +1,11 @@
 import asyncio
+import json
 import math
 
 import discord
 
 from discord.ext import commands
+from discord_slash import cog_ext, SlashContext
 from sqlalchemy import and_
 
 from ssm import session
@@ -13,6 +15,9 @@ from ssm.modules.embed_manager import EmbedManager
 from ssm.sql.models.blog import BlogsServer, BlogsCategory, BlogsChannel, BlogsUser
 
 xp_increase = 12
+
+with open(f'template/blog_achievement.json', encoding="utf-8") as f:
+	get_achievement = json.load(f)
 
 
 async def get_blog_info(ctx):
@@ -38,6 +43,10 @@ class BlogLevelSystem:
 		await db_manager.commit(setattr(self.search_user, 'xp', f'{remaining_xp}'), commit_type='update', show_commit_log=False)
 		embed = await EmbedManager().generate(embed_title='ブログユーザーレベルアップ！', embed_description=f'ブログでのユーザーレベルが{current_level} -> {update_level}に上がりました！おめでとうございます！', embed_content=[])
 		await self.ctx.channel.send(embed=embed)
+		if get_achievement.get('level').get(f'{update_level}'):
+			achievement = get_achievement.get('level').get(f'{update_level}')
+			embed = await EmbedManager().generate(embed_title=f'実績`{achievement.get("title")}`を達成', embed_description=f'{achievement.get("description")}', embed_content=[])
+			await self.ctx.channel.send(embed=embed)
 		return update_level
 
 	async def xp_up(self):
@@ -48,7 +57,17 @@ class BlogLevelSystem:
 	async def post_count_up(self):
 		update_post_count = self.search_user.post_count + 1
 		await db_manager.commit(setattr(self.search_user, 'post_count', f'{update_post_count}'), commit_type='update', show_commit_log=False)
+		if get_achievement.get('post_count').get(f'{update_post_count}'):
+			achievement = get_achievement.get('post_count').get(f'{update_post_count}')
+			embed = await EmbedManager().generate(embed_title=f'実績`{achievement.get("title")}`を達成', embed_description=f'{achievement.get("description")}', embed_content=[])
+			await self.ctx.channel.send(embed=embed)
 		return update_post_count
+
+	async def word_check(self):
+		if get_achievement.get('word').get(f'{self.ctx.content}'):
+			achievement = get_achievement.get('word').get(f'{self.ctx.content}')
+			embed = await EmbedManager().generate(embed_title=f'実績`{achievement.get("title")}`を達成', embed_description=f'{achievement.get("description")}', embed_content=[])
+			await self.ctx.channel.send(embed=embed)
 
 	async def check(self):
 		"""2500 * 5^2"""
@@ -57,6 +76,7 @@ class BlogLevelSystem:
 		update_xp = await self.xp_up()
 		if update_xp >= next_level_up_xp:
 			update_level = await self.levelup(next_level_up_xp, update_xp)
+		await self.word_check()
 
 
 class BlogCog(commands.Cog):
@@ -84,26 +104,29 @@ class BlogCog(commands.Cog):
 		embed = await EmbedManager().generate(embed_title='登録に成功', embed_description=f'{ctx.channel.category.name}をブログカテゴリとして登録しました', embed_content=[])
 		await ctx.send(embed=embed)
 
-	@blog.command()
-	async def setup(self, ctx):
-		search_category = session.query(BlogsCategory).filter(BlogsCategory.category_id == ctx.channel.category_id).first()
-		search_channel = session.query(BlogsChannel).filter(BlogsChannel.channel_id == ctx.channel.id).first()
+	@cog_ext.cog_subcommand(base="blog", name="setup", guild_ids=[530299114387406860], description='指定、実行したチャンネルをブログとして登録します')
+	async def setup(self, ctx: SlashContext, channel: discord.abc.GuildChannel = None):
+		if not channel:
+			channel = ctx.channel
+
+		search_category = session.query(BlogsCategory).filter(BlogsCategory.category_id == channel.category_id).first()
+		search_channel = session.query(BlogsChannel).filter(BlogsChannel.channel_id == channel.id).first()
 		if search_category:
 			if search_channel:
 				owner = self.bot.get_user(search_channel.owner_id)
 				embed = await EmbedManager().generate(embed_title='登録に失敗',
-				                                      embed_description=f'`{ctx.channel.category.name}`は既に{owner.name}さんが既にブログとして登録しているチャンネルです。他のチャンネルをお使いになるか解除をお願いしてください。',
+				                                      embed_description=f'`{channel.category.name}`は既に{owner.name}さんが既にブログとして登録しているチャンネルです。他のチャンネルをお使いになるか解除をお願いしてください。',
 				                                      embed_content=[], mode='failed')
 				await EmbedManager().send(ctx, embed, True, 5)
 				return
-			await db_manager.commit(BlogsChannel(category_id=ctx.channel.category.id, channel_id=ctx.channel.id, owner_id=ctx.author.id))
-			await db_manager.commit(BlogsUser(user_id=ctx.author.id, channel_id=ctx.channel.id))
+			await db_manager.commit(BlogsChannel(category_id=channel.category.id, channel_id=channel.id, owner_id=ctx.author.id))
+			await db_manager.commit(BlogsUser(user_id=ctx.author.id, channel_id=channel.id))
 			embed = await EmbedManager().generate(embed_title='登録に成功',
-			                                      embed_description=f'`{ctx.channel.name}`をブログとして登録しました。ようこそ{ctx.author.name}さん！',
+			                                      embed_description=f'`{channel.name}`をブログとして登録しました。ようこそ{ctx.author.name}さん！',
 			                                      embed_content=[], mode='succesed')
 		else:
 			embed = await EmbedManager().generate(embed_title='登録に失敗',
-			                                      embed_description=f'`{ctx.channel.category.name}`はブログカテゴリとして登録されていません。\nブログを登録するには`{bot_prefix}blog register category`を設定したいカテゴリ内のチャンネルで実行してください。※管理者権限が必要です',
+			                                      embed_description=f'`{channel.category.name}`はブログカテゴリとして登録されていません。\nブログを登録するには`{bot_prefix}blog register category`を設定したいカテゴリ内のチャンネルで実行してください。※管理者権限が必要です',
 			                                      embed_content=[], mode='failed')
 		await ctx.send(embed=embed)
 
@@ -111,11 +134,12 @@ class BlogCog(commands.Cog):
 	async def user(self, ctx):
 		await self.not_found_group(ctx)
 
-	@user.command(name='profile')
-	async def profile(self, ctx):
+	@cog_ext.cog_subcommand(base="blog", subcommand_group='user', name="profile", guild_ids=[530299114387406860], description='ブログでのプロフィールを表示します')
+	async def profile(self, ctx: SlashContext):
 		search_channel, search_user = await get_blog_info(ctx)
 		if not search_channel:
-			embed = await EmbedManager().generate(embed_title='プロフィールの表示に失敗', embed_description=f'`{ctx.channel.name}`はブログとして登録されていません。\n`{bot_prefix}blog setup`を実行したうえで再度実行してください`', embed_content=[], mode='failed')
+			embed = await EmbedManager().generate(embed_title='プロフィールの表示に失敗', embed_description=f'`{ctx.channel.name}`はブログとして登録されていません。\n`{bot_prefix}blog setup`を実行したうえで再度実行してください`', embed_content=[],
+			                                      mode='failed')
 			await EmbedManager().send(ctx, embed, True, 3)
 			return
 		elif not search_user:
